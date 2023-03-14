d928096c5438a3e10fd640e535af4c56d041da27
Akshansh0812 <akshanshanand2001@gmail.com>
2023-03-14 15:08:48 +0530

    Change the thread of a coroutine

--- app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -5,16 +5,18 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var messageTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         val textView = findViewById<TextView>(R.id.tvCount)
+        messageTextView = findViewById(R.id.tvMessage)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
 
@@ -25,11 +27,15 @@ class MainActivity : AppCompatActivity() {
             CoroutineScope(Dispatchers.IO).launch {
                 downloadUserData()
             }
-        } 
+        }
     }
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for (i in 1..200000) {
             Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                messageTextView.text = "Downloading user $i"
+            }
+            delay(100)
         }
     }
 }
